a8a6a34e14109ca4cfc54f1b3099928a04f39042
Noora <A2102257@edu.lapinamk.fi>
2021-09-13 15:35:40 +0300

    Exercise 1 done

--- exercise1.py
+++ exercise1.py
@@ -1,1 +1,23 @@
-s = input("Give me a string")
+
+def get_first_char(string):
+    return s[0]
+
+def get_last_char(string):
+    return  s[len(s)-1]
+
+s = input("Give me a string: ")
+length = len(s)
+length_without_spaces = len(s.replace(" ", ""))
+print(f"Length: {length}")
+print(f"Length without spaces: {length_without_spaces}")
+
+if s:
+    print(f"First character: {get_first_char(s)}")
+    print(f"Last character: {get_last_char(s)}")
+
+    l = s.lower()
+    if l[::-1] == l[0::]:
+        print(f"{s} is a palindrome")
+    else:
+        print(f"{s} is not a palindrome")
+
