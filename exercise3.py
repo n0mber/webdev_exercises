f9dc50924f9b10cbdfcbfe33673cd1bbbf5e8c45
Noora <A2102257@edu.lapinamk.fi>
2021-09-23 11:25:44 +0300

    exercise3 done

---
+++ exercise3.py
@@ -0,0 +1,31 @@
+
+def number_guess (number):
+    answer = input(f"Is the number {guess} (y/n)?")
+    return answer
+
+print("Think of an integer between 1 and 100. Don't cheat.")
+
+guess = int(100/2)
+guess_count = 1
+user_input = number_guess(guess)
+
+while user_input.lower() == "n":
+    lower_higher = input(f"Is real number smaller than {guess}?")
+    if lower_higher == "y":
+        guess =int((guess+1)/2)
+        guess_count += 1
+        user_input = number_guess(guess)
+
+    elif lower_higher == "n":
+        guess = int((101 + guess)/2)
+        guess_count += 1
+        user_input = number_guess(guess)
+
+    else:
+        quit()
+
+if not user_input:
+    quit()
+
+else:
+    print(f"I guessed correct in {guess_count} attempts.")
